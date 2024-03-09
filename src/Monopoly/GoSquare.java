3524db2a65c8455547d16a9c6b4878aff44925b1
Jouri <121612918+Jourialgh@users.noreply.github.com>
2024-03-09 23:43:08 +0300

    Update GoSquare.java

--- src/Monopoly/GoSquare.java
+++ src/Monopoly/GoSquare.java
@@ -7,11 +7,15 @@ package monopoly2;
 
 import java.util.ArrayList;
 
+//concrete product
 public class GoSquare extends Square {
 
     public GoSquare(Type type, String name, int position) {
         super(type, name, position);
     }
+
+    @Override
+    void takeProperties(ArrayList<Player> player, int order){}
 
     @Override
     void doTask(ArrayList<Player> player, int order, int numberOfSquare, int jailPosition) {
