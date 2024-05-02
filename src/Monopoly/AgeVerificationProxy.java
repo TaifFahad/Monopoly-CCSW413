b63ae64012e3b33b44b50acb60f8954c223f8ba8
Jouri <121612918+Jourialgh@users.noreply.github.com>
2024-05-02 15:44:05 +0300

    Update AgeVerificationProxy.java

--- src/Monopoly/AgeVerificationProxy.java
+++ src/Monopoly/AgeVerificationProxy.java
@@ -1,1 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package monopoly2;
+
+/**
+ *
+ * @author pca
+ */
+
+//Proxy class
+public class AgeVerificationProxy implements MonopolyGame {
+    private Scanner scanner;
+
+    public AgeVerificationProxy() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    @Override
+    public void startGame() {
+        System.out.print("Please enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+
+        if (age >= 12) {
+            MonopolyGame realGame = new MonopolyGameAL();
+            realGame.startGame();
+        } else {
+            System.out.println("Sorry, you are not old enough to play the game.");
+        }
+    }
+}
 
