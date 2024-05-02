e14a56d53ad860b512fa13cb48ee18d37b7d8b11
Jouri <121612918+Jourialgh@users.noreply.github.com>
2024-05-02 15:45:05 +0300

    Update Main.java
    Final commit for the proxy pattern

--- src/Monopoly/Main.java
+++ src/Monopoly/Main.java
@@ -1,1 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package monopoly2;
+
+/**
+ *
+ * @author pca
+ */
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      MonopolyGame game = new AgeVerificationProxy();
+        game.startGame();
+    }
+}
 
