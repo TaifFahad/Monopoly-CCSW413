c5717a2caddad6156bc237b068ae1f9f5c87eaa7
Jouri <121612918+Jourialgh@users.noreply.github.com>
2024-05-02 15:42:41 +0300

    Update MonopolyGameAL.java

--- src/Monopoly/MonopolyGameAL.java
+++ src/Monopoly/MonopolyGameAL.java
@@ -1,1 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package monopoly2;
 
+/**
+ *
+ * @author pca
+ */
+
+
+//Real subject class
+public class MonopolyGameAL implements MonopolyGame {
+    @Override
+    public void startGame() {
+        Board newGame = Board.createInstance();
+        newGame.startGame();
+    }
+}
