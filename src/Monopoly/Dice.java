4ffe697945247b398012a77cdbc7488898826ec2
TaifFahad <142192971+TaifFahad@users.noreply.github.com>
2024-05-17 00:33:44 +0300

    Update Dice.java
    Observer Pattern

--- src/Monopoly/Dice.java
+++ src/Monopoly/Dice.java
@@ -8,13 +8,43 @@ package monopoly2;
  *
  * @author pca
  */
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class Dice {
-
+//A dice interface to implement observer on it
+ interface Dice {
+    void rigesterObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers(int diceValue);
+    int rollDice();
+}
+//concrete class
+class ConcreteDice implements Dice{
+   public ConcreteDice()
+    {
+             this.observers = new ArrayList<>();
+    }
+     private List<Observer> observers;
     private Random randomDice = new Random();
 
-    public int rollDice() {
-        return randomDice.nextInt(6) + 1;
+public int rollDice() {
+    return randomDice.nextInt(6) + 1;
+}
+ @Override
+    public void rigesterObserver(Observer observer) {
+      observers.add(observer);
+      
+     }
+
+    @Override
+    public void removeObserver(Observer observer) {
+       observers.remove(observer); }
+
+    @Override
+     public void notifyObservers(int diceValue) {
+        for (Observer observer : observers) {
+            observer.updateDiceRoll(diceValue);
+        }
     }
 }
