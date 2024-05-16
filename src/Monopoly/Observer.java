4a1e7569088988a57f3bdaaea0a2bebbb996f299
TaifFahad <142192971+TaifFahad@users.noreply.github.com>
2024-05-17 00:28:03 +0300

    Create Observer.java
    Observer Pattern

---
+++ src/Monopoly/Observer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package monopoly2;
+
+/**
+ * @author pca
+ */
+
+//Interface for the observer pattern
+interface Observer {
+    
+    void updateDiceRoll(int diceValue);
+
+}
